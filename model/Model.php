2143dbe48d5941d543b3c503b613c4a3b7d5bdd5
Cyboulette <cyboulette58@gmail.com>
2016-11-26 12:27:49 +0100

    Save() et delete() Générique
    (c'est le but de cette classe)

--- model/Model.php
+++ model/Model.php
@@ -28,17 +28,17 @@ class Model
         $table_name = static::$object;
         $class_name = 'Model'.ucfirst($table_name);
 
-        try
-        {
+        try {
             $rep=Model::$pdo->query("SELECT * FROM gh_" .$table_name);
             $rep->setFetchMode(PDO::FETCH_CLASS,$class_name);
             $tab = $rep->FetchAll();
             return $tab;
-        }
-        catch(PDOException $e)
-        {
-            if (Conf::getDebug())   echo $e->getMessage();
-            else                    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+        } catch(PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            } else {
+                echo "Une erreur est survenue ! Merci de réessayer plus tard";
+            }
             die();
         }
     }
@@ -68,8 +68,7 @@ class Model
         $class_name = 'Model'.ucfirst(static::$object);
         $primary_key = static::$primary;
 
-        try
-        {
+        try {
             $sql = "SELECT * from $table_name WHERE $primary_key=:primary_val";
             $req_prep = Model::$pdo->prepare($sql);
 
@@ -83,11 +82,99 @@ class Model
             } else {
                 return $tab[0];
             }
+        } catch(PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            } else {
+                echo "Une erreur est survenue ! Merci de réessayer plus tard";
+            }
+            die();
         }
-        catch(PDOException $e)
-        {
-            if (Conf::getDebug())   echo $e->getMessage();
-            else                    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+    }
+
+    /*
+        Fonction générique qui permet d'insérer dans une table
+        $data = les colonnes de la table avec les valeurs associées
+        $typeReturn = NULL par défaut, ou id, si id, retourne le dernier id ajouté
+    */
+    public static function save($data, $typeReturn = NULL) {
+        try {
+            $sql = 'INSERT INTO `'.static::$tableName.'` VALUES (';
+
+            foreach ($data as $key => $value) {
+                $sql .= ':'.$key.',';
+            }
+
+            $sql = substr($sql, 0, -1);
+            $sql .= ')';
+
+            $add = Model::$pdo->prepare($sql);
+            $add->execute($data);
+            if($typeReturn == 'id') {
+                $lastId = Model::$pdo->lastInsertId();
+                return $lastId;
+            } else {
+                return true;
+            }
+        } catch(PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            }
+            return false;
+            die();
+        }
+    }
+
+    /*
+        Fonction générique qui permet de supprimer de la table du model courant la valeur de la clé primaire
+        $data = valeur de la clé primaire
+    */
+    public static function delete($data) {
+        $table_name = static::$tableName;
+        $primary_key = static::$primary;
+        try {
+          $sql = "DELETE FROM `".$table_name."` WHERE `".$primary_key."` = :".$primary_key."";
+          $rep = Model::$pdo->prepare($sql);
+          $values = array(
+            $primary_key => $data
+          );
+          $rep->execute($values);
+          return true;
+        } catch(PDOException $e) {
+          if (Conf::getDebug()) {
+            echo $e->getMessage();
+          }
+          return false;
+          die();
+        }
+    }
+
+
+    /*
+        Fonction générique qui permet de sélectionner tous résultats pour une clé particulière et sa valeur ($data) associée
+        $cle = le nom de la colonne à vérifier
+        $data = la valeur
+    */
+    public static function selectCustom($cle, $data) {
+        $table_name = static::$tableName;
+        $class_name = 'Model'.ucfirst(static::$object);
+        try {
+            $sql = "SELECT * from `".$table_name."` WHERE `".$cle."` = :".$cle."";
+            $req_generique = Model::$pdo->prepare($sql);
+
+            $values = array(
+                $cle => $data
+            );
+
+            $req_generique->execute($values);
+            $req_generique->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            $tab_gen = $req_generique->fetchAll();
+            return $tab_gen;
+        } catch(PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            }
+            return false;
             die();
         }
     }
@@ -110,11 +197,15 @@ class Model
             self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
-            if (Conf::getDebug())   echo $e->getMessage();
-            else                    echo "Une erreur est survenue ! Merci de réessayer plus tard";
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            } else {
+                echo "Une erreur est survenue ! Merci de réessayer plus tard";
+            }
             die();
         }
-
     }
+
 }
+
 Model::Init();
