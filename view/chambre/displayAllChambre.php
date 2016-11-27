0423e334958518d918a2f5439b24072693dfcc8a
Raziel-11 <florian-11@hotmail.fr>
2016-11-27 12:10:51 +0100

    glyphicon-eur

--- view/chambre/displayAllChambre.php
+++ view/chambre/displayAllChambre.php
@@ -9,7 +9,7 @@
 		       		<div class='col-xs-6 col-sm-3 placeholder'>
     					<img src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' width='200' height='200' class='img-responsive' alt='Generic placeholder thumbnail'>
        		 			<h4>{$nom}</h4>
-        				<span class='text-muted'>{$prix}</span>
+        				<span class='text-muted'>{$prix} <span class='glyphicon glyphicon-eur' aria-hidden='true'></span></span>
     				</div>
 		        ";
 		    }
