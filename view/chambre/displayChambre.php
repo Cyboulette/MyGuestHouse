74550dfec649a43104f242b14744e11bf76ad6cf
Raziel-11 <florian-11@hotmail.fr>
2016-11-30 09:41:20 +0100

    visuel chambre

---
+++ view/chambre/displayChambre.php
@@ -0,0 +1,6 @@
+<div class="col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+  <?php
+    $nom = $chambre->get("nomChambre");
+    echo "<h1 class='page-header'>$nom</h1>";	
+  ?>
+</div>
