d9cb85b5bd01c8cb4b41c335576db4a5312cf2f5
Raziel-11 <florian-11@hotmail.fr>
2016-12-01 23:45:38 +0100

    carousel

--- view/chambre/displayChambre.php
+++ view/chambre/displayChambre.php
@@ -1,110 +1,183 @@
-<?php
-    // variables
-    $nom = $chambre->get("nomChambre");
-    $prix = $chambre->get("prixChambre");
-    $superficie = $chambre->get("superficieChambre");
-    $description = $chambre->get("descriptionChambre");
-?>
-
-<div class="col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 ">
-    <div class='page-header text-center'><?=$nom?></div>
-
-    <!-- Carousel -->
-    <? if (isset($tab_photo) && !empty($tab_photo)) { ?>
-
-    <div>
-
-        <?php foreach ($tab_photo as $key => $value) { ?>
-        <?=$tab_photo[$key][0]?>
-        <br>
-        <?php } ?>
-
-    </div>
-
-    <? } else { ?>
-
-          <div class="alert alert-danger">Il n'y a aucune photo pour linstant</div>
-
-    <? } ?>
-
-    <!-- Description de la chambre -->
-    <div class='descriptionChambre'>
-        <h4> Descriptions </h4>
-        <ul>
-            <li> Nom de la chambre : <?=$nom?> </li>
-            <li> Prix<small>/nuit</small> : <?=$prix?> &euro; </li>
-            <li> Superficie : <?=$superficie?> m<sup>2</sup> </li>
-            <li> Description :
-                <ul>
-                    <li> <?=$description?> </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-
-    <!-- Details de la chambre -->
-    <? if (isset($tab_detail) && !empty($tab_detail)) { ?>
-
-    <div >
-        <h4>Détails </h4>
-        <ul>
-            <?php foreach ($tab_detail as $key => $value) { ?>
-            <li> <?=$tab_detail[$key][0]?> : <?=$tab_detail[$key][1]?> </li>
-            <?php } ?>
-        </ul>
-
-    </div>
-
-    <? } else { ?>
-
-    <div class="alert alert-danger">Il n'y a pas de details pour cette chambre</div>
-
-    <? } ?>
-
-    <!--Prestations de la chambre-->
-    <? if (isset($tab_prestation) && !empty($tab_prestation)) { ?>
-
-        <div>
-            <div>
-                <h4> Prestation </h4>
-            </div>
-            <div>
-                <ul>
-
-                <?php foreach ($tab_prestation as $key => $value) { ?>
-                    <li> <?=$tab_prestation[$key][0]?> : <?=$tab_prestation[$key][1]?> </li>
-                <?php } ?>
-
-                </ul>
-            </div>
-        </div>
-
-    <? } else { ?>
-
-        <div class="alert alert-danger">Il n'y a pas de prestations pour cette chambre</div>
-    <? } ?>
+<div class="col-xs-12 col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2"> 
+   
+    <?php   
+        // variables 
+        $nom = $chambre->get("nomChambre"); 
+        $prix = $chambre->get("prixChambre"); 
+        $superficie = $chambre->get("superficieChambre"); 
+        $description = $chambre->get("descriptionChambre"); 
+    ?> 
+ 
+ 
+    <?php 
+        // titre de la page 
+        echo "<h1 class='page-header'>{$nom}</h1>";   
+    ?> 
+ 
+ 
+    <?php  
+        //photo avec une futur carousel 
+        if (isset($tab_photo) && !empty($tab_photo)) { 
+            echo "
+                <div id='myCarousel' class='carousel slide' data-ride='carousel'>
+                    <ol class='carousel-indicators'>
+            ";
 
 
-    <?php //TODO : calendar -> resarvation ?>
+            foreach ($tab_photo as $key => $value) { 
+                $photo = $tab_photo[$key][0];
+                echo "
+                        <li data-target='#myCarousel' data-slide-to=$key
+                ";
+                if ($key == 0) {
+                    echo "
+                        class='active'></li>
+                    ";
+                }else{
+                    echo "
+                        ></li>
+                    ";
+                }
+            } 
 
-    <?php //TODO : bouton pour l'admin ??????? ?>
+            echo "
+                </ol>
+                <div class='carousel-inner' role='listbox'>
+            ";
 
-</div>
+            foreach ($tab_photo as $key => $value) { 
+                $photo = $tab_photo[$key][0];
+                echo "
+                    <div class='item  
+                ";
+                if ($key == 0) {
+                    echo "
+                         active'>
+                        <img src='{$photo}'>
+                    </div>
+                    ";
+                }else{
+                    echo "
+                        '>
+                        <img src='{$photo}'>
+                    </div>
+                    ";
+                }
+            }
+
+            echo "
+                  </div>
+                  <a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>
+                    <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
+                    <span class='sr-only'>Previous</span>
+                  </a>
+                  <a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'>
+                    <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
+                    <span class='sr-only'>Next</span>
+                  </a>
+                </div>
+            ";
+        }else{ 
+          echo '<div class="alert alert-danger">'."il n'y a aucune photo pour linstant".'</div>'; 
+        } 
+    ?> 
+ 
+ 
+    <?php  
+        // description de la chambre 
+        echo " 
+            <div class='descriptionChambre'> 
+              <div> 
+                <h4>Descriptions :<h4> 
+              </div> 
+              <ul> 
+                <li> 
+                  Nom de la chambre : {$nom} 
+                </li> 
+                <li> 
+                  Prix<small>/nuit</small> : {$prix}&euro; 
+                </li> 
+                <li> 
+                  Superficie : {$superficie}m<sup>2</sup> 
+                </li> 
+                <li> 
+                  Description : 
+                  <ul> 
+                    <li> 
+                      {$description} 
+                    </li> 
+                  </ul> 
+                </li> 
+              </ul> 
+            </div> 
+        "; 
+    ?> 
+ 
+ 
+    <?php 
+        // Details de la chambre 
+        if (isset($tab_detail) && !empty($tab_detail)) { 
+          echo "<div>"; 
+     
+            echo " 
+              <div> 
+                <h4>Détails :<h4> 
+              </div> 
+              <div> 
+                <ul> 
+            "; 
+     
+            foreach ($tab_detail as $key => $value) { 
+              echo "<li>".$tab_detail[$key][0]." : ".$tab_detail[$key][1]."</li>"; 
+            } 
+     
+            echo " 
+                </ul> 
+              </div> 
+            "; 
+     
+          echo "</div>"; 
+        }else{ 
+          echo '<div class="alert alert-danger">'."il n'y a pas de details pour cette chambre".'</div>'; 
+        } 
+    ?> 
+ 
+ 
+    <?php 
+        // Prestations de la chambre 
+        if (isset($tab_prestation) && !empty($tab_prestation)) { 
+          echo "<div>"; 
+     
+            echo " 
+              <div> 
+                <h4>Prestation :<h4> 
+              </div> 
+              <div> 
+                <ul> 
+            "; 
+     
+            foreach ($tab_prestation as $key => $value) { 
+              echo "<li>".$tab_prestation[$key][0]." : ".$tab_prestation[$key][1]."</li>"; 
+            } 
+     
+            echo " 
+                </ul> 
+              </div> 
+            "; 
+     
+          echo "</div>"; 
+        }else{ 
+          echo '<div class="alert alert-danger">'."il n'y a pas de prestations pour cette chambre".'</div>'; 
+        } 
+    ?> 
+ 
+    <?php 
+        //TODO : calendar -> resarvation 
+    ?> 
+     
+    <?php 
+        //TODO : bouton pour l'admin ??????? 
+    ?> 
 
 
-<!-- description de la chambre en div -->
-<!-- 
-	<div>
-		<p>- Nom de la chambre : {$nom}</p>
-		<p>- Prix<small>/nuit</small> : {$prix}</p>
-		<p>- Suerficie : {$superficie}</p>
-		<div>
-			<p>- Description : </p>
-			<p>{$description}</p>
-		</div>
-	</div>
- -->
-
-
-
-
+</div> 
