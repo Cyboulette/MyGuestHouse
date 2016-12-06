1f3e4c58d9e18549b7c0559ea2fb30c2f943a9d0
Cyboulette <cyboulette58@gmail.com>
2016-12-06 20:22:03 +0100

    CSS Responsive Menu

--- view/themes/admin/view.php
+++ view/themes/admin/view.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,6 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- THEME CUSTOM -->
     <link href="view/themes/admin/css/style.css" rel="stylesheet">
@@ -50,11 +51,11 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li <?php ControllerDefault::active('admin', 'index'); ?>><a href="index.php?controller=admin&action=index">Résumé <span class="sr-only">(current)</span></a></li>
-            <li <?php ControllerDefault::active('admin', 'utilisateurs'); ?>><a href="index.php?controller=admin&action=utilisateurs"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Utilisateur <span class="sr-only">(current)</span></a></li>
-            <li <?php ControllerDefault::active('admin', 'chambres'); ?>><a href="index.php?controller=admin&action=chambres"><span class="glyphicon glyphicon-lamp" aria-hidden="true"></span> Chambre <span class="sr-only">(current)</span></a></li>
-            <li <?php ControllerDefault::active('admin', 'reservations'); ?>><a href="index.php?controller=admin&action=reservations"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Réservation <span class="sr-only">(current)</span></a></li>
-            <li <?php ControllerDefault::active('admin', 'options'); ControllerDefault::active('admin', 'updateOptions');?>><a href="index.php?controller=admin&action=options"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Options <span class="sr-only">(current)</span></a></li>
+            <li <?php ControllerDefault::active('admin', 'index'); ?>><a href="index.php?controller=admin&action=index"><i class="fa fa-home" aria-hidden="true"></i> Résumé</a></li>
+            <li <?php ControllerDefault::active('admin', 'utilisateurs'); ?>><a href="index.php?controller=admin&action=utilisateurs"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Utilisateurs</a></li>
+            <li <?php ControllerDefault::active('admin', 'chambres'); ?>><a href="index.php?controller=admin&action=chambres"><span class="glyphicon glyphicon-lamp" aria-hidden="true"></span> Chambres</a></li>
+            <li <?php ControllerDefault::active('admin', 'reservations'); ?>><a href="index.php?controller=admin&action=reservations"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Réservations</a></li>
+            <li <?php ControllerDefault::active('admin', 'options'); ControllerDefault::active('admin', 'updateOptions');?>><a href="index.php?controller=admin&action=options"><i class="fa fa-cogs" aria-hidden="true"></i> Options</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
--- view/themes/default/view.php
+++ view/themes/default/view.php
@@ -14,10 +14,15 @@
 
     <body>
         <nav class="navbar navbar-inverse  menuHaut">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class="navbar-brand visible-xs" href="index.php">LOGO</a>
-                </div>
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand visible-xs" href="index.php"><?=$websiteName?></a>
             </div>
 
             <div id="navbar" class="collapse navbar-collapse">
@@ -28,16 +33,20 @@
                     <li <?php ControllerDefault::active('default', 'aboutus'); ?>><a href="index.php?controller=default&action=aboutus">À propos de nous</a></li>
 
                     <?php
-                    if(!isset($_SESSION['login'])) {
+                    if(!ControllerUtilisateur::isConnected()) {
                     ?>
                         <li <?php ControllerDefault::active('utilisateur', 'connect'); ?> ><a href="index.php?controller=utilisateur&action=connect">Connexion</a></li>
                         <li <?php ControllerDefault::active('utilisateur', 'register'); ?> ><a href="index.php?controller=utilisateur&action=register">Inscription</a></li>
                     <?php
                     } else {
+                        $currentUser = ModelUtilisateur::selectCustom('idUtilisateur', $_SESSION['idUser'])[0];
                     ?>
                         <li <?php ControllerDefault::active('commande', ''); ?> ><a href="index.php?controller=commande&action=readAll">Réservations</a></li>
                         <li <?php ControllerDefault::active('utilisateur', 'profil'); ?> ><a href="index.php?controller=utilisateur&action=profil">Profil</a></li>
                         <li <?php ControllerDefault::active('utilisateur', 'disconnect'); ?> ><a href="index.php?controller=utilisateur&action=disconnect">Déconnexion</a></li>
+                        <?php if($currentUser->getPower() == Conf::$power['admin']) { ?>
+                            <div class="goAdmin"><a href="index.php?controller=admin&action=index" class="btn btn-red"><i class="fa fa-lock" aria-hidden="true"></i> Administration</a></div>
+                        <?php } ?>
                     <?php
                         }
                     ?>
