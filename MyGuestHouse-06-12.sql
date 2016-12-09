51a24e31cdffd7f5950bf273eaf350e7c0b90e40
pcm <lamouche444@hotmail.fr>
2016-12-09 23:48:03 +0100

    Update MyGuestHouse-06-12.sql
    Delete contenu de la table `GH_Reservations`

--- MyGuestHouse-06-12.sql
+++ MyGuestHouse-06-12.sql
@@ -168,16 +168,6 @@ CREATE TABLE `GH_Reservations` (
   `dateFin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `GH_Reservations`
---
-
-INSERT INTO `GH_Reservations` (`idReservation`, `idChambre`, `idUtilisateur`, `dateDebut`, `dateFin`) VALUES
-(1, '1', '1', '2016-12-06', '2016-12-07'),
-(2, '2', '1', '2016-12-06', '2016-12-07'),
-(3, '3', '1', '2016-12-06', '2016-12-07');
-
-
 -- --------------------------------------------------------
 
 --
