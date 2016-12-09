37fe908ca3a64c55e7692df427cc29b9fe5dd242
pcm <lamouche444@hotmail.fr>
2016-12-09 12:37:11 +0100

    Update viewAllReservation.php
    Correction des hyper liens

--- view/admin/viewAllReservation.php
+++ view/admin/viewAllReservation.php
@@ -5,10 +5,10 @@
 
     <div class="col-xs-12 row placeholders">
         <ul class="nav nav-tabs" role="tablist">
-            <li><a href="index.php?controller=admin&action=displayAllReservation&mode=encours" > En cours <span class="badge">  <?php echo modelReservation::getNombreReservationEnCours() ?> </span></a></li>
-            <li><a href="index.php?controller=admin&action=displayAllReservation&mode=enattente" > En attente <span class="badge">  <?php echo modelReservation::getNombreReservationEnAttente() ?> </span></a></li>
-            <li><a href="index.php?controller=admin&action=displayAllReservation&mode=finis" > Finis <span class="badge">  <?php echo modelReservation::getNombreReservationFinis() ?> </span></a></li>
-            <li><a href="index.php?controller=admin&action=displayAllReservation&mode=annulees" > Annulées <span class="badge">  <?php echo modelReservation::getNombreReservationProbleme() ?> </span></a></li>
+            <li><a href="index.php?controller=admin&action=reservations&mode=encours" > En cours <span class="badge">  <?php echo modelReservation::getNombreReservationEnCours() ?> </span></a></li>
+            <li><a href="index.php?controller=admin&action=reservations&mode=enattente" > En attente <span class="badge">  <?php echo modelReservation::getNombreReservationEnAttente() ?> </span></a></li>
+            <li><a href="index.php?controller=admin&action=reservations&mode=finis" > Finis <span class="badge">  <?php echo modelReservation::getNombreReservationFinis() ?> </span></a></li>
+            <li><a href="index.php?controller=admin&action=reservations&mode=annulees" > Annulées <span class="badge">  <?php echo modelReservation::getNombreReservationProbleme() ?> </span></a></li>
         </ul>
 
 
