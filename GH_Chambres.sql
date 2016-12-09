962485069525c76470acd2c9a4e9b55fb6a1a2ee
pcm <lamouche444@hotmail.fr>
2016-12-09 23:50:26 +0100

    Update GH_Chambres.sql
    fail

--- GH_Chambres.sql
+++ GH_Chambres.sql
@@ -1,8 +1,8 @@
 --
--- Contenu de la table `GH_Reservations`
+-- Contenu de la table `GH_Chambres`
 --
 
-INSERT INTO `GH_Reservations` (`idReservation`, `idChambre`, `idUtilisateur`, `dateDebut`, `dateFin`) VALUES
-(1, '1', '1', '2016-12-06', '2016-12-07'),
-(2, '2', '1', '2016-12-06', '2016-12-07'),
-(3, '3', '1', '2016-12-06', '2016-12-07');
+INSERT INTO `gh_chambres`(`idChambre`, `nomChambre`, `descriptionChambre`, `prixChambre`, `superficieChambre`) VALUES 
+('1','chambre 1','blablablba',100,22),
+('2','chambre 2','blablablba',100,22),
+('3','chambre 3','blablablba',100,22);
