e16f1aade89413ab998cb864aaaa722135dfd058
pcm <lamouche444@hotmail.fr>
2016-12-09 23:50:07 +0100

    Update MyGuestHouse-06-12.sql
    Delete contenu de la table `GH_Chambres`

--- MyGuestHouse-06-12.sql
+++ MyGuestHouse-06-12.sql
@@ -47,15 +47,6 @@ CREATE TABLE `GH_Chambres` (
   `superficieChambre` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `GH_Chambres`
---
-
-INSERT INTO `gh_chambres`(`idChambre`, `nomChambre`, `descriptionChambre`, `prixChambre`, `superficieChambre`) VALUES 
-('1','chambre 1','blablablba',100,22),
-('2','chambre 2','blablablba',100,22),
-('3','chambre 3','blablablba',100,22);
-
 -- --------------------------------------------------------
 
 --
