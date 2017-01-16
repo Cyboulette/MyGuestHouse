e01efe871e3920726b9182d5a73e04a2be5068dc
Cyboulette <cyboulette58@gmail.com>
2017-01-16 18:41:40 +0100

    CorrectionMail

--- controller/ControllerMail.php
+++ controller/ControllerMail.php
@@ -1,39 +1,27 @@
 <?php
-require_once File::build_path(array('lib', 'PHPMailer', 'PHPMailerAutoload.php'));
-
 class ControllerMail {
 
   private static $email_source = 'myguesthouse.group@gmail.com'; // Indiquez votre adresse e-mail SMTP
-  private static $email_passwd = 'socap123'; // Indiquez votre mot de passe
-  private static $host = 'smtp.gmail.com'; // Indiquez l'hôte SMTP au besoin
 
   public static function sendMail($destination, $sujet, $contenu) {
     if(filter_var($destination, FILTER_VALIDATE_EMAIL)) {
       if(!empty($contenu) && !ctype_space($contenu)) {
-        $mail = new PHPMailer;
-        $mail->isSMTP();
-        $mail->Host = static::$host;
-        $mail->SMTPAuth = true;
-        $mail->Username = static::$email_source;
-        $mail->Password = static::$email_passwd;
-        $mail->SMTPSecure = 'tls'; // Changez la sécurité SMTP au besoin
-        $mail->Port = 587; // Changez le port SMTP au besoin
-        $mail->isHTML(true);
-        $mail->CharSet = 'UTF-8';
+        // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-        $mail->setFrom(static::$email_source, 'MyGuestHouse');
-        $mail->addAddress($destination);
-        $mail->Subject = htmlspecialchars($sujet);
-        $mail->Body = $contenu;
+        // En-têtes additionnels
+        $headers .= 'To: <'.$destination.'>' . "\r\n";
+        $headers .= 'From: MyGuestHouse <'.static::$email_source.'>' . "\r\n";
 
-        if(!$mail->send()) {
-          return false;
-        } else {
+        // Envoi
+        if(@mail($destination, '=?utf-8?B?'.base64_encode($sujet).'?=', $contenu, $headers)) {
           return true;
+        } else {
+          return false;
         }
       }
     }
   }
-
 }
 ?>
